9ff28b0e154c2b26cd350cb06890ec7a3d2e854e
Sharmaji95 <59778771+Sharmaji95@users.noreply.github.com>
2023-01-09 17:55:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $amount=$_GET['am'];
-$link = "upi://pay?pa=2223330091834461@RATN0VAAPIS.ifsc.npci&pn=Dues&tn=Loan_Dues&am=";
+$link = "upi://pay?pa=rpy.qrvaloremsta13298438@icici&pn=Dues&tn=Loan_Dues&tr=RZPK3TKR1sgdwaLlDqrv2&cu=INR&mode=02&am=";
 $newURL = $link.$amount;
 If($amount){
     echo $newURL;
@@ -12,4 +12,4 @@ If($amount){
 
 ?>
 
-// billbook8781224@yesbank 2223330091834461@RATN0VAAPIS.ifsc.npci&cu=INR
+// billbook8781224@yesbank (2223330091834461@RATN0VAAPIS.ifsc.npci&cu=INR). __(upi://pay?pa=rpy.qrvaloremsta13298438@icici&pn=YOGESH%20&tn=YOGESH%20&tr=RZPK3TKR1sgdwaLlDqrv2&cu=INR&mode=02&mbbnumber=9068062563)
