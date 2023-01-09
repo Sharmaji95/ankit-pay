46bce8a8d8c367baf7b4c28da3998b45d08d5ab4
Sharmaji95 <59778771+Sharmaji95@users.noreply.github.com>
2023-01-09 22:21:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $amount=$_GET['am'];
-$link = "upi://pay?pa=rpy.qrvaloremsta13298438@icici&pn=Dues&tn=Loan_Dues&tr=RZPK3TKR1sgdwaLlDqrv2&cu=INR&mode=02&am=";
+$link = "upi://pay?pa=rpy.qrvaloremsta13298438@icici&pn=Zest-Money&tn=Loan_Dues&tr=RZPK3TKR1sgdwaLlDqrv2&cu=INR&mode=02&am=";
 $newURL = $link.$amount;
 If($amount){
     echo $newURL;
